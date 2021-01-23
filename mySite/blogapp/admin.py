9362331a26c1074708ae825ca109765ee57e726e
karlSartorio <karlSartorio@users.noreply.github.com>
2021-01-23 17:20:37 +0000

    set up the models in the register.py

--- mySite/blogapp/admin.py
+++ mySite/blogapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blogapp.models import Post, Comment
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+
+#create a super user.
+# Username:     karl
+# email:       k@rl.com
+#password:     testPassword
